b5b39a1c0638a637429e52ecaaea89a9e6b4fc55
AaronKimutai <aaronkimutai24@gmail.com>
2025-11-05 14:15:18 +0300

    Updated socket.js

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 import { useEffect, useState } from 'react';
 
-const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || 'http://localhost:5000';
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL 
 
 export const socket = io(SOCKET_URL, {
     autoConnect: false,
